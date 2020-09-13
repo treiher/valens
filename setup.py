3d51033814202f8a19ce5f5ba85ef1ef5f7b452e
Tobias Reiher <15232394+treiher@users.noreply.github.com>
2020-09-13 15:37:29 +0200

    Add development dependencies to setup script

--- setup.py
+++ setup.py
@@ -12,5 +12,17 @@ setup(
     packages=find_packages(where="valens"),
     python_requires=">=3.6, <4",
     install_requires=["matplotlib", "pandas"],
+    extras_require={
+        "devel": [
+            "black ==20.8b1",
+            "flake8 >=3",
+            "isort >=5",
+            "mypy >=0.770",
+            "pylint >=2.6.0",
+            "pytest >=5",
+            "pytest-cov >=2.10.0",
+            "pytest-xdist >=1.32.0",
+        ]
+    },
     entry_points={"console_scripts": ["valens=valens.cli:main"]},
 )

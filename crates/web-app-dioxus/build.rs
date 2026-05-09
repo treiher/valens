ba2ac7d7c7a0e4be71f122613e4fc605fbba88ab
Tobias Reiher <15232394+treiher@users.noreply.github.com>
2026-05-09 16:24:04 +0200

    Ensure web-app-dioxus is rebuilt when version changes

--- crates/web-app-dioxus/build.rs
+++ crates/web-app-dioxus/build.rs
@@ -1,6 +1,7 @@
 #![deny(clippy::pedantic)]
 
 fn main() {
+    println!("cargo:rerun-if-env-changed=VALENS_VERSION");
     let version = std::env::var("VALENS_VERSION").unwrap_or("dev".to_string());
     println!("cargo:rustc-env=VALENS_VERSION={version}");
 }
